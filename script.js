4fc22422e83b579262249f0670d6c5a1dfcb3118
Adewunmi Ademola <152196581+Adewunmi-Ademola@users.noreply.github.com>
2024-06-23 06:48:40 +0100

    Update script.js
    fetch

--- script.js
+++ script.js
@@ -178,22 +178,8 @@ function showMessage() {
 }
 
 
-/**----------------------wordpress fetch----------------------------------------------- */
+/**----------------------wordpress fetch----------------------------------------------- *
 
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('http://localhost:10010/wp-json/wp/v2/posts')
-        .then(response => response.json())
-        .then(data => {
-            const content = document.getElementById('content');
-            data.forEach(post => {
-                const postElement = document.createElement('div');
-                postElement.innerHTML = `
-                    <h2>${post.title.rendered}</h2>
-                    <div>${post.content.rendered}</div>
-                `;
-                content.appendChild(postElement);
-            });
-        });
-});
+
 
 
